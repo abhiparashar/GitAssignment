5d0e9efb7a859fd284c1bac9dbcc1842c6e19fc1
abhiparashar <aparashar103@gmail.com>
2021-06-25 11:49:55 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// Pages Components
 import Home from './pages/Home';
 import User from './pages/User';
 
-// Layout Components
 import Navbar from './layout/Navbar';
 
-// Redux Store
 import { Provider } from 'react-redux';
 import store from './store';
 
-// Material UI
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App = () => {
